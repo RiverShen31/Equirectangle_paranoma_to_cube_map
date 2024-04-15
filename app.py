0171cf84b8ff7a70c772b9d3b346a021a9b9c26b
RiverShen31 <tienlam3105@gmail.com>
2024-04-15 10:22:31 +0700

    Create 4 folder

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
-import cv2
 import os
-from math import pi, sin, cos, tan, atan2, hypot, floor
+from math import pi, atan2, hypot, floor
 from numpy import clip
-import sys
 from PIL import Image
+import shutil  # Import shutil module for archiving
+import zipfile
+import itertools
+import time
 
 app = Flask(__name__)
 
@@ -89,14 +91,95 @@ def convert_to_cubemap(image_path):
     imgOut.save(output_path)
     return output_path
 
-if not os.path.exists(app.config['UPLOAD_FOLDER']):
-    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+def split_cubemap(cubemap_path):
+    cubemap = Image.open(cubemap_path)
+    width, height = cubemap.size
+    face_size = width // 4
+
+    faces = {
+        "Top": cubemap.crop((face_size * 2, 0, face_size * 3, face_size)),
+        "Right": cubemap.crop((0, face_size, face_size, face_size*2)),
+        "Left": cubemap.crop((face_size, face_size, face_size*2, face_size*2)),
+        "Back": cubemap.crop((face_size * 2, face_size, face_size*3, face_size*2)),
+        "Front": cubemap.crop((face_size*3, face_size*1, face_size*4, face_size*2)),
+        "Down": cubemap.crop((face_size*2, face_size*2, face_size*3, face_size*3)),
+    }
+
+    output_folder = os.path.splitext(cubemap_path)[0] + "_faces"
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    for face_name, face_image in faces.items():
+        face_image.save(os.path.join(output_folder, f"{face_name.lower()}_face.png"))
+
+    return output_folder
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+def organize_images(media_folder):
+    for face_name in ['t', 'd', 'b', 'f', 'l', 'r']:
+        os.makedirs(os.path.join(media_folder, 'Level1', face_name), exist_ok=True)
+
+    for face_name in ['top', 'down', 'back', 'front', 'left', 'right']:
+        shutil.move(
+            os.path.join(media_folder, f"{face_name}_face.png"),
+            os.path.join(media_folder, 'Level1', face_name[0], f"{face_name}_image.png")
+        )
+
+    for face_name in ['top', 'down', 'back', 'front', 'left', 'right']:
+        img = Image.open(os.path.join(media_folder, 'Level1', face_name[0], f"{face_name}_image.png"))
+        width, height = img.size
+        tile_width = width // 2
+        tile_height = height // 2
+        for i, j in itertools.product(range(2), range(2)):
+            x1 = i * tile_width
+            y1 = j * tile_height
+            x2 = (i + 1) * tile_width
+            y2 = (j + 1) * tile_height
+            tile = img.crop((x1, y1, x2, y2))
+            os.makedirs(os.path.join(media_folder, 'Level2', face_name[0]), exist_ok=True)
+            tile.save(os.path.join(media_folder, 'Level2', face_name[0], f"{face_name}_{i}{j}.png"))
+
+    for face_name in ['top', 'down', 'back', 'front', 'left', 'right']:
+        img = Image.open(os.path.join(media_folder, 'Level1', face_name[0], f"{face_name}_image.png"))
+        width, height = img.size
+        tile_width = width // 3
+        tile_height = height // 3
+        for i, j in itertools.product(range(3), range(3)):
+            x1 = i * tile_width
+            y1 = j * tile_height
+            x2 = (i + 1) * tile_width
+            y2 = (j + 1) * tile_height
+            tile = img.crop((x1, y1, x2, y2))
+            output_folder = os.path.join(media_folder, 'Level3', face_name[0])
+            os.makedirs(output_folder, exist_ok=True)
+            tile.save(os.path.join(output_folder, f"{face_name}_{i}{j}.png"))
+
+    for face_name in ['top', 'down', 'back', 'front', 'left', 'right']:
+        img = Image.open(os.path.join(media_folder, 'Level1', face_name[0], f"{face_name}_image.png"))
+        width, height = img.size
+        tile_width = width // 5
+        tile_height = height // 5
+        for i, j in itertools.product(range(5), range(5)):
+            x1 = i * tile_width
+            y1 = j * tile_height
+            x2 = (i + 1) * tile_width
+            y2 = (j + 1) * tile_height
+            tile = img.crop((x1, y1, x2, y2))
+            output_folder = os.path.join(media_folder, 'Level4', face_name[0])
+            os.makedirs(output_folder, exist_ok=True)
+            tile.save(os.path.join(output_folder, f"{face_name}_{i}{j}.png"))
+
+
+    # Create a zip file containing the contents of the media folder
+    zip_path = shutil.make_archive(media_folder, 'zip', media_folder)
+
+    # Return the path to the zip file
+    return zip_path
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -105,11 +188,24 @@ def upload():
         filename = os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(file.filename))
         print("Uploaded filename:", filename)  # Add this line for debugging
         file.save(filename)
-        output_path = convert_to_cubemap(filename)
-        return send_file(output_path, as_attachment=True)
+        cubemap_path = convert_to_cubemap(filename)
+        output_folder = split_cubemap(cubemap_path)
+
+        media_folder = os.path.join(output_folder, 'media')
+        if not os.path.exists(media_folder):
+            os.makedirs(media_folder)
+
+        for item in os.listdir(output_folder):
+            shutil.move(os.path.join(output_folder, item), media_folder)
+
+        zip_path = organize_images(media_folder)
+
+        # Remove the original media_folder
+        shutil.rmtree(media_folder)
+
+        return send_file(zip_path, as_attachment=True)
     else:
         return "No file uploaded!"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
